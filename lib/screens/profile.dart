56fdd65b252d3dceae071c4a2e339f36ef11ec12
diwasneupane <diwas.neupane55@gmail.com>
2022-11-17 14:41:56 +0545

    Profile section ui completed

--- lib/screens/profile.dart
+++ lib/screens/profile.dart
@@ -1,3 +1,4 @@
+import 'package:feedtheneed/screens/login.dart';
 import 'package:flutter/material.dart';
 
 class Profile extends StatefulWidget {
@@ -12,106 +13,56 @@ class _ProfileState extends State<Profile> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color(0xFFFAFAFA),
-      body: Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const SizedBox(
-              height: 20,
-            ),
-            const Text(
-              "Profile",
-              style: TextStyle(
-                fontSize: 30,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            const SizedBox(
-              height: 40,
-            ),
-            Container(
-              height: 95,
-              width: double.infinity,
-              decoration: BoxDecoration(
-                color: const Color(0xFF41A2CD),
-                borderRadius: BorderRadius.circular(20),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.black.withOpacity(0.25),
-                    spreadRadius: 0,
-                    blurRadius: 4,
-                    offset: const Offset(0, 4), // changes position of shadow
-                  ),
-                ],
-              ),
-              child: Center(
-                child: ListTile(
-                  leading: const CircleAvatar(
-                    radius: 30,
-                    backgroundColor: Colors.white,
-                    child: CircleAvatar(
-                      radius: 27,
-                      backgroundImage: NetworkImage(
-                          'https://static01.nyt.com/images/2022/09/16/arts/16CAMERON1/16CAMERON1-videoSixteenByNine3000.jpg'),
-                    ),
-                  ),
-                  title: const Text(
-                    "Email",
-                    style: TextStyle(color: Colors.white),
-                  ),
-                  subtitle: const Text("Username",
-                      style: TextStyle(color: Colors.white)),
-                  trailing: IconButton(
-                    onPressed: () {
-                      //action coe when button is pressed
-                    },
-                    icon: const Icon(
-                      Icons.edit,
-                      color: Colors.white,
-                    ),
+      body: SingleChildScrollView(
+        child: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Text(
+                  "Profile",
+                  style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
                   ),
                 ),
-              ),
-            ),
-            const SizedBox(
-              height: 30,
-            ),
-            Container(
-              height: 80,
-              width: double.infinity,
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(20),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.black.withOpacity(0.09),
-                    spreadRadius: 0,
-                    blurRadius: 44,
-                    offset: const Offset(0, 4), // changes position of shadow
+                const SizedBox(
+                  height: 20,
+                ),
+                Container(
+                  height: 95,
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    color: const Color(0xFF41A2CD),
+                    borderRadius: BorderRadius.circular(20),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black.withOpacity(0.25),
+                        spreadRadius: 0,
+                        blurRadius: 4,
+                        offset:
+                            const Offset(0, 4), // changes position of shadow
+                      ),
+                    ],
                   ),
-                ],
-              ),
-              child: Center(
-                child: Column(
-                  children: [
-                    ListTile(
+                  child: Center(
+                    child: ListTile(
                       leading: const CircleAvatar(
                         radius: 30,
                         backgroundColor: Colors.white,
                         child: CircleAvatar(
                           radius: 27,
-                          child: Icon(
-                            Icons.person_outline,
-                            color: Colors.white,
-                          ),
+                          backgroundImage: NetworkImage(
+                              'https://static01.nyt.com/images/2022/09/16/arts/16CAMERON1/16CAMERON1-videoSixteenByNine3000.jpg'),
                         ),
                       ),
                       title: const Text(
                         "Email",
+                        style: TextStyle(color: Colors.white),
                       ),
                       subtitle: const Text("Username",
-                          style: TextStyle(color: Colors.blueGrey)),
+                          style: TextStyle(color: Colors.white)),
                       trailing: IconButton(
                         onPressed: () {
                           //action coe when button is pressed
@@ -122,11 +73,244 @@ class _ProfileState extends State<Profile> {
                         ),
                       ),
                     ),
-                  ],
+                  ),
                 ),
-              ),
-            )
-          ],
+                const SizedBox(
+                  height: 30,
+                ),
+                Container(
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(20),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black.withOpacity(0.09),
+                        spreadRadius: 0,
+                        blurRadius: 44,
+                        offset:
+                            const Offset(0, 4), // changes position of shadow
+                      ),
+                    ],
+                  ),
+                  child: Center(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const SizedBox(
+                          height: 20,
+                        ),
+                        ListTile(
+                          leading: const CircleAvatar(
+                            radius: 30,
+                            backgroundColor: Colors.white,
+                            child: CircleAvatar(
+                              backgroundColor:
+                                  Color.fromARGB(255, 241, 250, 253),
+                              radius: 27,
+                              child: Icon(
+                                Icons.person_outline,
+                                color: Color(0xFF41A2CD),
+                              ),
+                            ),
+                          ),
+                          title: const Text(
+                            "My Account ",
+                          ),
+                          subtitle: const Text("Make changes to your account",
+                              style: TextStyle(
+                                  color: Colors.blueGrey, fontSize: 10)),
+                          trailing: IconButton(
+                            onPressed: () {
+                              //action coe when button is pressed
+                            },
+                            icon: const Icon(
+                              Icons.keyboard_arrow_right_outlined,
+                              color: Colors.blueGrey,
+                            ),
+                          ),
+                        ),
+                        ListTile(
+                          leading: const CircleAvatar(
+                            radius: 30,
+                            backgroundColor: Colors.white,
+                            child: CircleAvatar(
+                              backgroundColor:
+                                  Color.fromARGB(255, 241, 250, 253),
+                              radius: 27,
+                              child: Icon(
+                                Icons.auto_graph,
+                                color: Color(0xFF41A2CD),
+                              ),
+                            ),
+                          ),
+                          title: const Text(
+                            "Donation Point ",
+                          ),
+                          subtitle: const Text("View your donation point",
+                              style: TextStyle(
+                                  color: Colors.blueGrey, fontSize: 10)),
+                          trailing: IconButton(
+                            onPressed: () {
+                              //action coe when button is pressed
+                            },
+                            icon: const Icon(
+                              Icons.keyboard_arrow_right_outlined,
+                              color: Colors.blueGrey,
+                            ),
+                          ),
+                        ),
+                        InkWell(
+                          onTap: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => const Login(),
+                              ),
+                            );
+                          },
+                          child: ListTile(
+                            leading: const CircleAvatar(
+                              radius: 30,
+                              backgroundColor: Colors.white,
+                              child: CircleAvatar(
+                                backgroundColor:
+                                    Color.fromARGB(255, 241, 250, 253),
+                                radius: 27,
+                                child: Icon(
+                                  Icons.logout_outlined,
+                                  color: Color(0xFF41A2CD),
+                                ),
+                              ),
+                            ),
+                            title: const Text(
+                              "Log out",
+                            ),
+                            subtitle: const Text(
+                                "Further secure your account for safety",
+                                style: TextStyle(
+                                    color: Colors.blueGrey, fontSize: 10)),
+                            trailing: IconButton(
+                              onPressed: () {
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) => const Login()),
+                                ); //action coe when button is pressed
+                              },
+                              icon: const Icon(
+                                Icons.keyboard_arrow_right_outlined,
+                                color: Colors.blueGrey,
+                              ),
+                            ),
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 20,
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                const Text(
+                  "More",
+                  style: TextStyle(
+                    fontSize: 15,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                Container(
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(20),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black.withOpacity(0.09),
+                        spreadRadius: 0,
+                        blurRadius: 44,
+                        offset:
+                            const Offset(0, 4), // changes position of shadow
+                      ),
+                    ],
+                  ),
+                  child: Center(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const SizedBox(
+                          height: 20,
+                        ),
+                        ListTile(
+                          leading: const CircleAvatar(
+                            radius: 30,
+                            backgroundColor: Colors.white,
+                            child: CircleAvatar(
+                              backgroundColor:
+                                  Color.fromARGB(255, 241, 250, 253),
+                              radius: 27,
+                              child: Icon(
+                                Icons.notifications_active_outlined,
+                                color: Color(0xFF41A2CD),
+                              ),
+                            ),
+                          ),
+                          title: const Text(
+                            "Help & Support",
+                          ),
+                          trailing: IconButton(
+                            onPressed: () {
+                              //action coe when button is pressed
+                            },
+                            icon: const Icon(
+                              Icons.keyboard_arrow_right_outlined,
+                              color: Colors.blueGrey,
+                            ),
+                          ),
+                        ),
+                        ListTile(
+                          leading: const CircleAvatar(
+                            radius: 30,
+                            backgroundColor: Colors.white,
+                            child: CircleAvatar(
+                              backgroundColor:
+                                  Color.fromARGB(255, 241, 250, 253),
+                              radius: 27,
+                              child: Icon(
+                                Icons.favorite_border,
+                                color: Color(0xFF41A2CD),
+                              ),
+                            ),
+                          ),
+                          title: const Text(
+                            "About App ",
+                          ),
+                          trailing: IconButton(
+                            onPressed: () {
+                              //action coe when button is pressed
+                            },
+                            icon: const Icon(
+                              Icons.keyboard_arrow_right_outlined,
+                              color: Colors.blueGrey,
+                            ),
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 20,
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
         ),
       ),
     );
